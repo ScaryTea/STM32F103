0bdd52ef317522a246d348be746b4f4316e2d896
Vlad <v.kotsiurba@gmail.com>
2018-02-17 21:38:12 +0200

    inter.c not working
    Signed-off-by: Vlad <v.kotsiurba@gmail.com>

--- inter.c
+++ inter.c
@@ -1,11 +1,12 @@
 #include "stm32f1xx.h"
 
 int main() {
-
-	 // enable tacting
+	// enable tacting
         RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+	RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
 	GPIOC->CRH &= ~GPIO_CRH_CNF13;
 	GPIOC->CRH = (GPIOC->CRH|GPIO_CRH_MODE13_1)&(~GPIO_CRH_MODE13_0);
@@ -18,9 +19,33 @@ int main() {
 	EXTI->IMR |= EXTI_IMR_MR6;
 	EXTI->FTSR |= EXTI_FTSR_TR6;
 
-
 	NVIC_EnableIRQ(EXTI9_5_IRQn);
-	while(1);
+	/*
+	uart_init();
+	char * s = "Hello";
+	uart_send(s); */
+
+// Usart starts here
+	GPIOA->CRL = (GPIOA->CRL|GPIO_CRL_CNF2_1)&(~GPIO_CRL_CNF2_0);
+        GPIOA->CRL = (GPIOA->CRL|GPIO_CRL_MODE2_1)&(~GPIO_CRL_MODE2_0);
+
+	//USART->CR1 &= ~USART->CR1_M;
+
+	USART2->BRR = 0x341;
+
+	USART2->CR1 |= USART_CR1_UE;
+
+	USART2->CR1 |= USART_CR1_TE;
+
+	USART2->DR = 't';
+
+	while(!(USART2->SR&USART_SR_TXE));
+
+	 USART2->DR = 'a';
+
+// Ends here
+
+	while(1) { for(volatile int i = 0; i < 5000000; i++); }
 }
 
 void EXTI9_5_IRQHandler(void) {
