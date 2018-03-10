076ff2c2ae0188604f8f6f7c9f59d363ba48e149
Vlad <v.kotsiurba@gmail.com>
2018-03-10 02:02:32 +0200

    Things fixed, RGB led added
    Next commit is about to contain .h files and convenient set
    of functions for RGB led and maybe smth more.
    
    Signed-off-by: Vlad <v.kotsiurba@gmail.com>

--- pwm.c
+++ pwm.c
@@ -1,15 +1,39 @@
 #include "stm32f1xx.h"
 
+
+
 int main() {
 	
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;	
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+
 	MODIFY_REG(GPIOA->CRL,
-			   GPIO_CRL_CNF0 | GPIO_CRL_MODE0,
-			   GPIO_CRL_CNF0_1 | GPIO_CRL_MODE0_1);
+	GPIO_CRL_CNF0 | GPIO_CRL_MODE0 | GPIO_CRL_CNF1 | GPIO_CRL_MODE1
+	| GPIO_CRL_CNF2 | GPIO_CRL_MODE2,
+	GPIO_CRL_CNF0_1 | GPIO_CRL_MODE0_1 | GPIO_CRL_CNF1_1 | GPIO_CRL_MODE1_1
+	| GPIO_CRL_CNF2_1 | GPIO_CRL_MODE2_1);
 
 	SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM2EN);
 	TIM2->PSC = 3;
+	TIM2->ARR = 99;
+	TIM2->CCR1 = 95; // blue
+	TIM2->CCR2 = 0; // green
+	TIM2->CCR3 = 0; // red
+	CLEAR_BIT(TIM2->CCMR1, TIM_CCMR1_CC1S | TIM_CCMR1_CC2S);
+	CLEAR_BIT(TIM2->CCMR2, TIM_CCMR2_CC3S);
+	SET_BIT(TIM2->CCMR1, 0x6 << TIM_CCMR1_OC1M_Pos | 0x6 << TIM_CCMR1_OC2M_Pos);
+	SET_BIT(TIM2->CCMR2, 0x6 << TIM_CCMR2_OC3M_Pos);
+	SET_BIT(TIM2->CCMR1, TIM_CCMR1_OC1PE | TIM_CCMR1_OC2PE);
+	SET_BIT(TIM2->CCMR2, TIM_CCMR2_OC3PE);
+	TIM2->CR1 |= TIM_CR1_ARPE;
+	TIM2->EGR |= TIM_EGR_UG;
+	TIM2->CR1 |= TIM_CR1_CEN;
+	SET_BIT(TIM2->CCER, TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E
+		| TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E);
+
+/*
+	SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM2EN);
+	TIM2->PSC = 0;
 	TIM2->ARR = 99;
 	TIM2->CCR1 = 30;
 	TIM2->CCMR1 &= ~TIM_CCMR1_CC1S;
@@ -19,16 +43,15 @@ int main() {
 	TIM2->EGR |= TIM_EGR_UG;
 	TIM2->CR1 |= TIM_CR1_CEN;
 	TIM2->CCER |= TIM_CCER_CC1E;
-
+*/
 	while(1) 
-	{
-		for(volatile int i = 0; i < 100; i++) {
-			TIM2->CCR1 = i;
-			for(volatile int y = 0; y < 1000; y++);
-		}
-		for(volatile int i = 99; i > 0; i--) {
-			TIM2->CCR1 = i;
-			for(volatile int y = 0; y < 1000; y++);
-		}
+	{/*
+		for(int i = 0; i < 270; i++)
+		{
+			TIM2->CCR1 = i < 90 ? i : 90-i; // blue
+			TIM2->CCR2 = (i > 90 && i < 180) ? 180-i : 0; // green
+			TIM2->CCR3 = (i > 180 && i < 270) ? 270-i : 0; // red
+	for(volatile int j = 0; j < 10000; j++);
+		}	*/				
 	} 
 }
