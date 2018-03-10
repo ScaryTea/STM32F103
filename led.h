97aed5c5505b8812e6810a94f201333ae43d2591
Vlad <v.kotsiurba@gmail.com>
2018-03-10 15:28:26 +0200

    Added .h files; RGBled added
    -pwm.c is just for pwm
    -led.c has led and RGBled functions
    -other minor improvements
    
    Signed-off-by: Vlad <v.kotsiurba@gmail.com>

---
+++ led.h
@@ -0,0 +1,10 @@
+#include "stm32f1xx.h"
+
+extern void led_init();
+extern void led_on();
+extern void led_off();
+extern void led_switch();
+
+extern void RGBled_init();
+extern void RGBled_set(unsigned char r, unsigned char g, unsigned char b);
+
