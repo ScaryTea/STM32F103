e48d970e2220e91f57ee067e5e8f979d48b50f55
Vlad <v.kotsiurba@gmail.com>
2018-04-21 17:52:28 +0300

    Added extern functions to lcd file
    -display_enable()
    -display_disable()
    
    Signed-off-by: Vlad <v.kotsiurba@gmail.com>

--- lcd.h
+++ lcd.h
@@ -14,6 +14,10 @@ extern void display_clear();
 
 extern void lcd_init();
 
+extern void display_enable();
+
+extern void display_disable();
+
 extern void display_send_byte(uint8_t c);
 
 extern void display_send_bytes(uint8_t* c, unsigned int n);
