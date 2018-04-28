bb974ecbdfd208c97ea6eb9a3ad3ab57d525239b
Vlad <v.kotsiurba@gmail.com>
2018-04-29 00:12:28 +0300

    Final version of formatter 'bmp to lcd banks'
    Signed-off-by: Vlad <v.kotsiurba@gmail.com>

--- format.c
+++ format.c
@@ -3,46 +3,37 @@
 #include <stdint.h>
 #include <string.h>
 
-uint8_t shifts[] = { 1<<7, 1<<6, 1<<5, 1<<4, 1<<3, 1<<2, 1<<1, 1 };
+uint8_t shifts[] = { 1, 1<<1, 1<<2, 1<<3, 1<<4, 1<<5, 1<<6, 1<<7 };
 
 uint32_t fsize = 0;
 unsigned char* readFile(char* path);
 void printBin(unsigned char c);
 
 int main(int argc, char *argv[]) {
-
-	unsigned char* input = readFile(argv[1]);
-	printf("file = %s\n",input);
-	uint32_t offset = input[10];
+	/* unused info, probably will need this in future
 	uint32_t width = input[18];
 	uint32_t height = input[22];
+	*/
+	unsigned char* input = readFile(argv[1]);
+	uint32_t offset = input[10];
+	printf("offset: (%u)\n fsize = %d\n",offset,fsize);
+
 	unsigned char arr[6][84];
 	for(int i = 0; i < 6; i++)
 		for(int j = 0; j < 84; j++)
 			arr[i][j] = 0;
-	printf("res: (%u)\n fsize = %d\n",offset,fsize);
-//	int k = fsize - 1;
 
 	unsigned char a;	
 	int i=0, j=83;
+	// v - vertical iterating
+	// k - iterating over bytes in 'strings' of 12 bytes
+	// bitpos - iterating over bits in byte
 	for(int v = 0; v < 48; v++, j=83, i=(int)(v/8)) {
-		//printf("v=%d,j=%d,i=%d\n",v,j,i);
 		for(int k = 1; k < 12; k++) {
-			//printf("k=%d,j=%d\n",k,j);
 			a = input[fsize-1 - v*12 - k];
-			//printBin(a);
-			for(int tmp = 0; tmp < 8; tmp++, j--) {
-				//printf("lowloop: j=%d, tmp=%d\n",j,tmp);
-				if(j < 0) break;
-				//printf("\n[[arr[%d][%d] before: ",i,j);
-				//printBin(arr[i][j]);
-				arr[i][j] |= (!!(a & (1 << tmp))) << v%8;
-				/*printf("\narr[%d][%d] after: ",i,j);
-				printBin(arr[i][j]);
-				printf("\na(): ");
-				printBin(a);
-				printf("\nv=%d; tmp=%d; k=%d",v,tmp,k);
-				printf("]]\n");*/
+			for(int bitpos = (k==1)?4:0; bitpos < 8; bitpos++, j--) {
+				if(j < 0) { printf("Smth broken\n"); break; }
+				arr[i][j] |= (!!(a & shifts[bitpos])) << v%8;
 			}
 		}
 	}
@@ -51,8 +42,6 @@ int main(int argc, char *argv[]) {
 		for(j = 0; j < 84; j++) {
 			fwrite(&arr[i][j], sizeof(unsigned char), 1, f);
 		}
-	fclose(f);
-
 }
 
 unsigned char* readFile(char* path) {
