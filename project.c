8b400e006476aff9cd491952e09dc21efae2a550
Vlad <v.kotsiurba@gmail.com>
2018-05-27 19:32:17 +0300

    Eventually working code
    Will be corrected soon
    
    Signed-off-by: Vlad <v.kotsiurba@gmail.com>

--- project.c
+++ project.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include "uart.h"
 #include "i2c.h"
 #include "spi.h"
 #include "lcd.h"
+#include <stdlib.h>
 
 void setup_pic_reciever();
 unsigned char get_angle(int16_t angle_value, int8_t z);
@@ -12,7 +12,7 @@ void delayMil(int millis) {
 }
 
 int main() {
-	I2C_init();	
+	I2C_init();
 	setup_pic_reciever();
 
 	//SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPAEN);
@@ -20,10 +20,8 @@ int main() {
 
 	I2C_write_byte(0x6B, 0); // MPU power on
 	unsigned char tab[] = {'|','\0'};
-	unsigned char cr[] = {'\n','\r','\0'};
-	unsigned char rotation[] = {'0','\0'}; 
-	// 0 - 0, 1 - 180, 2 - 270, 3 - 90
-	char c[18];
+	// rotation values: 0 - 0, 1 - 180, 2 - 270, 3 - 90
+	unsigned char rotation[] = {'?','\0'}; 
 	unsigned char prev_angle = '0', curr_angle = '0';
 
 	while(1) {
@@ -32,36 +30,23 @@ int main() {
 		uint8_t resL = I2C_read_byte(0x3E);
 		int8_t resZ = I2C_read_byte(0x3F);
 		curr_angle = get_angle((resH<<8)|resL,resZ);
-		if(curr_angle != prev_angle); {
+		//if(curr_angle != prev_angle); 
+		{
 			rotation[0] = curr_angle;
 			uart1_send(rotation);
 			delayMil(50);
-			uart1_send(tab);
+			//uart1_send(tab);
 		}
-		prev_angle = curr_angle;
-		int16_t ttt = (resH<<8)|resL;
-		sprintf (c, "%d",ttt);
-		uart1_send((unsigned char*)c);
-		delayMil(50);
-		uart1_send(tab);
-
-		sprintf (c, "%d",resZ);
-		uart1_send((unsigned char*)c);
-		delayMil(50);
-		uart1_send(tab);
-
-		delayMil(50);
-		uart1_send(cr);
-		
+		//prev_angle = curr_angle;		
 	}
 }
 
 unsigned char get_angle(int16_t angle_value, int8_t z) {
-	#define rot 8192
-	if(angle_value >= -13000 && angle_value <= 13000 && z > 0) return '0';
-	if(angle_value >= 13000 && angle_value <= 16384) return '1';
-	if(angle_value >= -13000 && angle_value <= 13000 && z < 0) return '2';
-	if(angle_value >= -16384 && angle_value <= -13000) return '3';
+	#define rot 13000
+	if(angle_value >= -rot && angle_value <= rot && z > 0) return '0';
+	if(angle_value >= rot) return '1';
+	if(angle_value >= -rot && angle_value <= rot && z < 0) return '2';
+	if(angle_value <= -rot) return '3';
 	return '0';
 }
 
@@ -78,9 +63,6 @@ void setup_pic_reciever() {
 	// enable DMA reciever for UART
 	SET_BIT(USART1->CR3, USART_CR3_DMAR);
 
-	// enable event generating for UART
-//	SET_BIT(USART1->CR1, USART_CR1_RXNEIE);
-
 	// set peripheral and memory addresses
 	DMA1_Channel5->CPAR = (uintptr_t) &(USART1->DR);
 	DMA1_Channel5->CMAR = (uintptr_t) &(SPI1->DR);
@@ -89,7 +71,7 @@ void setup_pic_reciever() {
 	DMA1_Channel5->CCR |= 3 << DMA_CCR_PL_Pos;
 	
 	// number of bytes to be transfered
-	DMA1_Channel5->CNDTR = 100800;
+	DMA1_Channel5->CNDTR = 10080;
 
 	MODIFY_REG(DMA1_Channel5->CCR, 
 		DMA_CCR_PSIZE | DMA_CCR_MSIZE | DMA_CCR_DIR,
